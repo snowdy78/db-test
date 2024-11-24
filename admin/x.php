2fdb3e5c4a3c59a5d97ea38a6ec571aa7c94b4d6
snowdy78 <themrleoner12@mail.ru>
2024-11-24 14:55:41 +0700

    фикс админки

---
+++ admin/x.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include "../php/components/head.php";
+    ?>
+</head>
+<body>
+    <?php
+        include "../php/components/header.php";
+    ?>
+    hello
+</body>
+</html>
